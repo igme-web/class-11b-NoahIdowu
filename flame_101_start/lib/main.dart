cf340b6cb518b6cb090d4d48d85e366465ad6da7
NoahIdowu <92756085+NoahIdowu@users.noreply.github.com>
2025-11-11 08:13:18 -0500

    Finished week 11b

--- flame_101_start/lib/main.dart
+++ flame_101_start/lib/main.dart
@@ -1,22 +1,42 @@
+import 'dart:async';
+import 'package:flame/events.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
+import 'package:flame/components.dart';
+import 'package:flame/palette.dart';
 import 'player.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(GameWidget(
+    game: FlameDemoGame(),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class FlameDemoGame extends FlameGame {
+  late Player player;
+  late JoystickComponent joystick;
+
+  final knobPaint = BasicPalette.red.withAlpha(200).paint();
+  final knobBgPaint = BasicPalette.red.withAlpha(100).paint();
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+  Future<void> onLoad() async {
+    await images.loadAll(["player.png"]);
+
+    joystick = JoystickComponent(
+      knob: CircleComponent(
+        radius: 15,
+        paint: knobPaint,
       ),
+      background: CircleComponent(
+        radius: 50,
+        paint: knobBgPaint,
+      ),
+      margin: const EdgeInsets.only(left: 20, bottom: 20),
     );
+
+    player = Player(joystick: joystick);
+    add(player);
+    add(joystick);
   }
 }
