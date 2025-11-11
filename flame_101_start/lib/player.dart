23b01a47ace67f18f7014febbe961235129bd647
NoahIdowu <92756085+NoahIdowu@users.noreply.github.com>
2025-11-11 08:15:03 -0500

    Finished week 11b

--- flame_101_start/lib/player.dart
+++ flame_101_start/lib/player.dart
@@ -1,1 +1,28 @@
-class Player {}
+import 'dart:async';
+import 'package:flame/components.dart';
+import 'main.dart';
+
+class Player extends SpriteComponent with HasGameReference<FlameDemoGame> {
+  final JoystickComponent joystick;
+
+  Player({required this.joystick});
+
+  @override
+  Future<void> onLoad() async {
+    sprite = Sprite(game.images.fromCache("player.png"));
+    anchor = Anchor.center;
+    height = 100;
+    width = 100;
+    position = Vector2(game.size.x / 2, game.size.y - 100);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    if (!joystick.delta.isZero()) {
+      position.add(joystick.relativeDelta * 300 * dt);
+      angle = joystick.delta.screenAngle();
+    }
+  }
+}
